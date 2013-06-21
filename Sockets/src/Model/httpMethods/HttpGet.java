65685539b087c7385a81ce58519105bbaab30d60
Iago Moreira da Silva <iago.silva@al.infnet.edu.br>
2013-06-20 23:46:57 -0300

    servidor montando o header Connection dinamicamente

--- Sockets/src/Model/httpMethods/HttpGet.java
+++ Sockets/src/Model/httpMethods/HttpGet.java
@@ -10,15 +10,22 @@ public class HttpGet
 
 		StringBuilder builder = new StringBuilder();
 		request = request.replace("\n\n", "\n");
-
-		builder.append("HTTP/1.1 200").append("\n")
-	   	   .append("Connection: Keep-Alive\n")
-	   	   .append("Date: Thu, 06 Aug 1998 12:00:15 GMT\n")
-	   	   .append("Server: Apache/1.3.0 (Unix)\n")
-	   	   .append("Last-Modified: Mon, 22 Jun 1998\n")
-	   	   .append("Content-Length: " + request.length() + "\n")
-	   	   .append("Content-Type: text/html;charset=utf-8\n")
-	   	   .append("\n");
+		
+		
+			System.out.println("FOI AQUI");
+		
+		builder.append("HTTP/1.1 200").append("\n");
+		if(request.contains("Keep-Alive"))
+			builder.append("Connection: Keep-Alive\n");
+		else
+			builder.append("Connection: Close\n");
+		
+   	   builder.append("Date: Thu, 06 Aug 1998 12:00:15 GMT\n")
+		   	   .append("Server: Apache/1.3.0 (Unix)\n")
+		   	   .append("Last-Modified: Mon, 22 Jun 1998\n")
+		   	   .append("Content-Length: " + request.length() + "\n")
+		   	   .append("Content-Type: text/html;charset=utf-8\n")
+		   	   .append("\n");
 		return builder.toString();
 	}
 
